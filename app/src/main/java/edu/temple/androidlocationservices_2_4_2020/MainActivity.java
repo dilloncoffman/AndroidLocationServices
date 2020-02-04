22a46c167d386b22360848bd207501502e96e3d3
Dillon Coffman <tug40394@temple.edu>
2020-02-04 16:13:56 -0500

    Check permissions

--- app/src/main/java/edu/temple/androidlocationservices_2_4_2020/MainActivity.java
+++ app/src/main/java/edu/temple/androidlocationservices_2_4_2020/MainActivity.java
@@ -1,14 +1,43 @@
 package edu.temple.androidlocationservices_2_4_2020;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationManager = getSystemService(LocationManager.class);
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 111);
+        } else {
+            showLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            showLocationUpdates();
+        }
+    }
+
+    private void showLocationUpdates() {
+
     }
 }
